4ec60ab18177489ba79e38bd1f7f66e33e4b3b7f
Ali T <ali.tarraf@gmail.com>
2021-03-10 15:51:58 -0800

    first commit

---
+++ gatsby-config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  siteMetadata: {
+    title: "RogueAI",
+  },
+  plugins: [
+    "gatsby-transformer-remark",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+  ],
+};
